dfe62fba18c8fb4688268e8b2fb3f9e4c3075050
Alexander <apvannug.lhl.webdev@gmail.com>
2022-12-12 00:31:47 -0700

    refactor with semi-colon to end statement

--- 1_queries/2_total_students_in_cohort.sql
+++ 1_queries/2_total_students_in_cohort.sql
@@ -1,3 +1,3 @@
 SELECT COUNT(*)
 FROM students
-WHERE cohort_id <= 3
+WHERE cohort_id <= 3;
--- 1_queries/4_students_without_gmail_phone.sql
+++ 1_queries/4_students_without_gmail_phone.sql
@@ -1,4 +1,4 @@
 SELECT id, name, email, cohort_id
 FROM students
 WHERE NOT email = '%@gmail.com'
-AND phone IS NULL
+AND phone IS NULL;
